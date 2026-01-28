f6d211395e34e2389730b43734e97e0d6ca248f0
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2026-01-28 22:42:20 +0530

    Docker started

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "web_server_public_ip" {
-  description = "The public IP address of the web server instance."
-  value       = aws_instance.web_server.public_ip
-}
+# output "web_server_public_ip" {
+#   description = "The public IP address of the web server instance."
+#   value       = aws_instance.web_server.public_ip
+# }
